e13f0a837409df5da4b03a761c032fd1922070a8
JonMcCutchen <jmmccutchen1@gmail.com>
2024-03-04 14:31:04 -0500

    api viewsets

--- wifi_db_backend/api/views.py
+++ wifi_db_backend/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from locations.models import Location
+from locations.serializers import LocationSerializer
+from speedtests.models import Speedtest
+from speedtests.serializers import SpeedtestSerializer
 
-# Create your views here.
+class LocationViewSet(viewsets.ModelViewSet):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+
+class SpeedtestViewSet(viewsets.ModelViewSet):
+    queryset = Speedtest.objects.all()
+    serializer_class = SpeedtestSerializer
--- wifi_db_backend/wifi_db_backend/settings.py
+++ wifi_db_backend/wifi_db_backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.gis',
+    'rest_framework',
+    'rest_framework.authtoken',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
