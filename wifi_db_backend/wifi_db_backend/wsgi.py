36890f42046d614f28c66893c9b739d57d3dc051
JonMcCutchen <jmmccutchen1@gmail.com>
2024-03-05 11:40:10 -0500

    debug

--- wifi_db_backend/wifi_db_backend/wsgi.py
+++ wifi_db_backend/wifi_db_backend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wifi_db_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wifi_db_backend.wifi_db_backend.settings')
 
 application = get_wsgi_application()
