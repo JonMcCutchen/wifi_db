95c9c3a62a84c82f932c74288c98c8039231ec8b
JonMcCutchen <jmmccutchen1@gmail.com>
2024-03-06 14:42:07 -0500

    new migrations

--- wifi_db_backend/locations/models.py
+++ wifi_db_backend/locations/models.py
@@ -2,5 +2,5 @@ from django.contrib.gis.db import models
 
 # Create your models here.
 class Location(models.Model):
-    place_name = models.CharField(max_length=200)
+    place_name = models.CharField(max_length=200 , null=True, blank=True)
     coordinates = models.PointField()
